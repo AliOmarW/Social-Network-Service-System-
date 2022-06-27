92933a93b4f159d3930061e0d3915ff611fec1e8
AliOmarW <86895340+AliOmarW@users.noreply.github.com>
2022-06-28 01:16:37 +0300

    small edit

--- SNS Project/Project Code.cs
+++ SNS Project/Project Code.cs
@@ -7,7 +7,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-// SSN  Project
+// SNS  Project.
 namespace Social_NetWork_Service
 {
     [Serializable]
